45444732914390acf1b0920db4f92d9f42801b0e
Mike Hume <MichaelSHume@icloud.com>
2021-06-29 19:56:00 -0700

    Added functionality to parse SQL database info to JS object array through PHP.

--- addBike.js
+++ addBike.js
@@ -1,4 +1,12 @@
-var color_sel = document.getElementById("color_sel").value;
+class Bike_Model {
+    constructor(id, name, cat_id) {
+        this.id = id;
+        this.name = name;
+        this.cat_id = cat_id;
+    }
+}
+
+var color_sel = document.getElementById("pri_color_sel").value;
 var sec_color_sel = document.getElementById("sec_color_sel").value;
 var category_sel = document.getElementById("category_sel").value;
 var model_sel = document.getElementById("model_sel").value;
@@ -6,21 +14,60 @@ var level_sel = document.getElementById("level_sel").value;
 var size_sel = document.getElementById("size_sel").value;
 var wheel_size_sel = document.getElementById("wheel_size_sel").value;
 
+function add_bike_field(bike_fields, new_field){
+    //alert("adding new field: " + new_field);
+    bike_fields.push(new_field);
+    return bike_fields;
+}
 
-function foo() {
-    alert("Submit button clicked!");
-    return true;
- }
+function create_bike_fields(){
+    //alert("creating bike_fields");
+    var bike_fields = [];
+    return bike_fields;
+}
+
+function print_bike_fields(bike_fields){
+    //alert("printing!");
+    alert("---> " + bike_fields);
+};
 
 
+
+function clickTest() {
+    //alert("Click Test Run");
+    //document.getElementById('category_sel').removeAttribute('disabled');
+    document.getElementById('category_sel').removeAttr('disabled');
+ };
+
+ function test_func(msg){
+    alert(msg);
+ };
+
+function disableFields(bike_fields){
+    
+    document.getElementById("category_sel").disabled = true;
+
+    bike_fields.forEach((cat) => {
+        document.getElementById(cat).disabled = true;
+
+    });
+    
+    
+};
+function enableFields(fields){
+    //alert("enableFields Run");
+    //document.getElementById("category_sel").removeAttribute('disabled');
+    //document.getElementById("category_sel").disabled = false;
+    curr_field = fields[0];
+    //alert("Current Field: " + curr_field);
+    document.getElementById(curr_field).disabled = false;
+    fields.shift();
+};
 
 
 
 $(document).ready(function(){
-    $('body').on('keyup', 'input.phone-input', function(){
-        if($(this).val().length === this.size){
-            var inputs = $('input.phone-input');
-            inputs.eq(inputs.index(this) + 1).focus();
-        }
-    });
+    $.fn.enableCategory = function() { 
+        document.getElementById('category_sel').removeAttribute('disabled');
+    }
 });
--- addBike.php
+++ addBike.php
@@ -5,14 +5,12 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Wheel Away</title>
+        <!--LINK JQUERY-->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
         <link rel="stylesheet" href="style.css">
-        <!--LINK JQUERY-->
-        <script type="text/javascript" src="jquery-3.3.1.js"></script>
-        <script src="https://ajaz.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-        <!--<script src="js/reg.js"></script>-->
-        <script src="js/addBike.js"></script>
+        <script type="text/javascript" src="addBike.js"></script>
     </head>
     <body>
         <nav class="navbar navbar-inverse">
@@ -45,158 +43,193 @@
             </div>
         </nav>
 
+        <?php
 
+            $servername = "localhost";
+            $username = "mike";
+            $password = "Bascmilacomah1!";
+            $dbname = "wacc";
+
+            // Create connection
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            // Check connection
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            } 
+
+            // Select Primary Color
+            $sql = "SELECT * FROM colors";
+            $pri_color_result = $conn->query($sql);
+            $json_pri_color_array = json_encode($pri_color_result);
+
+            // Select Secondary Color
+            $sql = "SELECT * FROM colors";
+            $sec_color_result = $conn->query($sql);
+            $json_sec_color_array = json_encode($sec_color_result);
+
+            // Select Categories
+            $sql = "SELECT * FROM categories";
+            $category_result = $conn->query($sql);
+            $json_category_array = json_encode($category_result);
+
+            // Select Model
+            $sql = "SELECT * FROM model";
+            $model_result = $conn->query($sql);
+            $json_model_array = json_encode($model_result);
+
+            // Select Level
+            $sql = "SELECT * FROM levels";
+            $level_result = $conn->query($sql);
+            $json_level_array = json_encode($level_result);
+
+            // Select Size
+            $sql = "SELECT * FROM size";
+            $size_result = $conn->query($sql);
+            $json_size_array = json_encode($size_result);
+
+            // Select Wheel Size
+            $sql = "SELECT * FROM wheel_sizes";
+            $wheel_size_result = $conn->query($sql);
+            $json_wheel_size_array = json_encode($json_size_array);
+        ?>
+
+        <script>
+            var model_obj_array = [];
+            <?php
+                while ($row = mysqli_fetch_array($model_result)) {
+                    echo 'temp_model = new Bike_Model(' . $row['model_id'] . ', "' . $row['name'] . '", ' . $row['cat_id'] . ', );';
+                    echo ' model_obj_array.push(temp_model);';
+                }
+            ?>
+            console.log(model_obj_array);
+        </script>
+
+
+
+        <script>
+            var category_obj = <?php echo $json_category_array; ?>;
+            var model_obj = <?php echo $json_model_array; ?>;
+            var level_obj = <?php echo $json_level_array; ?>;
+            var size_obj = <?php echo $json_size_array; ?>;
+            var wheel_size_obj = <?php echo $json_wheel_size_array; ?>;
+            var pri_color_obj = <?php echo $json_pri_color_array; ?>;
+            var sec_color_obj = <?php echo $json_sec_color_array; ?>;
+        </script>
 
         <form action="sendBike.php" method="post">
             <div class="form-body">
-                <!--<div class="row">-->
                     <div class="form-holder">
                         <div class="form-content">
                             <div class="form-items">
                                 <h3>Add New Bicycle</h3>
+                                <script>
+                                    //test_func();
+                                </script>
                                 <form class="requires-validation" novalidate>
-                                    <?php
-
-                                        $servername = "localhost";
-                                        $username = "mike";
-                                        $password = "Bascmilacomah1!";
-                                        $dbname = "wacc";
-
-                                        // Create connection
-                                        $conn = new mysqli($servername, $username, $password, $dbname);
-                                        // Check connection
-                                        if ($conn->connect_error) {
-                                            die("Connection failed: " . $conn->connect_error);
-                                        } 
-
-                                        // Select Primary Color
-                                        $sql = "SELECT * FROM colors";
-                                        $pri_color_result = $conn->query($sql);
-
-                                        // Select Secondary Color
-                                        $sql = "SELECT * FROM colors";
-                                        $sec_color_result = $conn->query($sql);
-
-                                        // Select Categories
-                                        $sql = "SELECT * FROM categories";
-                                        $category_result = $conn->query($sql);
-
-                                        // Select Model
-                                        $sql = "SELECT * FROM model";
-                                        $model_result = $conn->query($sql);
-
-                                        // Select Level
-                                        $sql = "SELECT * FROM levels";
-                                        $level_result = $conn->query($sql);
-
-                                        // Select Size
-                                        $sql = "SELECT * FROM size";
-                                        $size_result = $conn->query($sql);
-
-                                        // Select Wheel Size
-                                        $sql = "SELECT * FROM wheel_sizes";
-                                        $wheel_size_result = $conn->query($sql);
-
-                                        ?>
-
-                                        <select class="form-select mt-3" name="pri_color_sel" required>
-                                            <!--<option selected disabled id="pri_color_sel" name="pri_color_sel" value="pri_color_sel">Primary Color</option>-->
-                                            <?php
-                                                echo '<option value="">Primary Color</option>';
-                                                while ($row = mysqli_fetch_array($pri_color_result)) {
-                                                    echo '<option value="' . $row['color_id'] . '|' . $row['color'] . '">' . $row['color'] . '</option>';
-                                                }   
-                                            ?>
+                                        
+                                        <select class="form-select mt-3" name="model_year" id="model_year" required onchange=enableFields(temp_fields)>
+                                            <script> var bike_fields = create_bike_fields();
+                                                //alert("ALERT TEST");
+                                            </script>
+                                            <option value="" selected disabled>Model Year</option>
+                                            <option value="2020">2020</option>
+                                            <option value="2021">2021</option>
+                                            <option value="2022">2022</option>
                                         </select>
 
-                                        <select class="form-select mt-3" name="sec_color_sel" required>
-                                            <option value="">Secondary Color</option>
-                                            <?php
-                                                while ($row = mysqli_fetch_array($sec_color_result)) {
-                                                    echo '<option value="' . $row['color_id'] . '|' . $row['color'] . '">' . $row['color'] . '</option>';
-                                                }
-                                            ?>
-                                        </select>
-
-                                        <select class="form-select mt-3" name="category_sel" required>
-                                            <option value="">Category</option>
+                                        <select class="form-select mt-3" id="category_sel" name="category_sel" required onchange=enableFields(temp_fields)>
+                                        
+                                            <option value="" selected >Category</option>
                                             <?php
                                                 while ($row = mysqli_fetch_array($category_result)) {
                                                     echo '<option value="' . $row['cat_id'] . '|' . $row['category'] . '">' . $row['category'] . '</option>';
                                                     //echo "<option value='" . $row['cat_id'] . "'>" . $row['category'] . "</option>";
                                                 }
                                             ?>
-                                        </select>
+                                            <script>bike_fields = add_bike_field(bike_fields, "category_sel"); </script>
 
-                                        <select class="form-select mt-3" name="model_sel"required>
-                                            <option value="">Model</option>
+
+                                        </select>
+                                        
+
+                                        <select class="form-select mt-3" name="model_sel" id="model_sel" required onchange=enableFields(temp_fields)>
+                                            <option value="" selected disabled>Model</option>
                                             <?php
                                                 while ($row = mysqli_fetch_array($model_result)) {
                                                     echo '<option value="' . $row['model_id'] . '|' . $row['name'] . '">' . $row['name'] . '</option>';
                                                     //echo "<option value='" . $row['model_id'] . "'>" . $row['name'] . "</option>";
                                                 }
                                             ?>
+                                            <script>bike_fields = add_bike_field(bike_fields, "model_sel");</script>
                                         </select>
 
-                                        <select class="form-select mt-3" name="level_sel" required>
-                                            <option value="">Level</option>
+                                        <select class="form-select mt-3" name="level_sel" id="level_sel" required onchange=enableFields(temp_fields)>
+                                            <option value="" selected disabled>Level</option>
                                             <?php
                                                 while ($row = mysqli_fetch_array($level_result)) {
                                                     echo '<option value="' . $row['level_id'] . '|' . $row['level'] . '">' . $row['level'] . '</option>';
                                                     //echo "<option value='" . $row['level_id'] . "'>" . $row['level'] . "</option>";
                                                 }
                                             ?>
+                                            <script>bike_fields = add_bike_field(bike_fields, "level_sel");</script>
                                         </select>
 
-                                        <select class="form-select mt-3" name="size_sel" required>
-                                            <option value="">Size</option>
+                                        <select class="form-select mt-3" name="size_sel" id="size_sel" required onchange=enableFields(temp_fields)>
+                                            <option value="" selected disabled>Size</option>
                                             <?php
                                                 while ($row = mysqli_fetch_array($size_result)) {
                                                     echo '<option value="' . $row['size_id'] . '|' . $row['size'] . '">' . $row['size'] . '</option>';
                                                     //echo "<option value='" . $row['size_id'] . "'>" . $row['size'] . "</option>";
                                                 }
                                             ?>
+                                            <script>bike_fields = add_bike_field(bike_fields, "size_sel");</script>
                                         </select>
 
-                                        <select class="form-select mt-3" name="wheel_size_sel" required>
-                                            <option value="">Wheel Size</option>
+                                        <select class="form-select mt-3" name="wheel_size_sel" id="wheel_size_sel" required onchange=enableFields(temp_fields)>
+                                            <option value="" selected disabled>Wheel Size</option>
                                             <?php
                                                 while ($row = mysqli_fetch_array($wheel_size_result)) {
                                                     echo '<option value="' . $row['wheel_size_id'] . '|' . $row['wheel_size'] . '">' . $row['wheel_size'] . '</option>';
                                                     //echo "<option value='" . $row['wheel_size_id'] . "'>" . $row['wheel_size'] . "</option>";
                                                 }
                                             ?>
+                                            <script>bike_fields = add_bike_field(bike_fields, "wheel_size_sel")</script>
                                         </select>
 
-                                        <select class="form-select mt-3" name="model_year" required>
-                                            <option value="">Model Year</option>
-                                            <option value="2020">2020</option>
-                                            <option value="2021">2021</option>
-                                            <option value="2022">2022</option>
+                                        <select class="form-select mt-3" name="pri_color_sel" id="pri_color_sel" required onchange=enableFields(temp_fields)>
+                                            <!--<option selected disabled id="pri_color_sel" name="pri_color_sel" value="pri_color_sel">Primary Color</option>-->
+                                            <option value="" selected disabled>Primary Color</option>
+                                            <?php
+                                                while ($row = mysqli_fetch_array($pri_color_result)) {
+                                                    echo '<option value="' . $row['color_id'] . '|' . $row['color'] . '">' . $row['color'] . '</option>';
+                                                }   
+                                            ?>
+                                            <script>bike_fields = add_bike_field(bike_fields, "pri_color_sel")</script>
                                         </select>
 
-                                        <!--<div class="form-select mt-3" required>-->
-                                            <!--<label for="part_num">Part Number</label>-->
-                                            <input type="text" class="form-select mt-3" id="sku" name="sku" placeholder="SKU">
-                                        <!--</div>-->
+                                        <select class="form-select mt-3" name="sec_color_sel" id="sec_color_sel" required onchange=enableFields(temp_fields)>
+                                            <option value="" selected disabled>Secondary Color</option>
+                                            <?php
+                                                while ($row = mysqli_fetch_array($sec_color_result)) {
+                                                    echo '<option value="' . $row['color_id'] . '|' . $row['color'] . '">' . $row['color'] . '</option>';
+                                                }
+                                            ?>
+                                            <script>bike_fields = add_bike_field(bike_fields, "sec_color_sel")</script>
+                                        </select>
 
-                                        <div class="form-select mt-3" required>
-                                            <!--<label for="part_num">Part Number</label>-->
-                                            <input type="text" class="form-control" id="part_num" name="part_num" placeholder="Part Number">
-                                        </div>
+                                        <input type="text" class="form-select mt-3" id="sku" name="sku" placeholder="SKU" oninput=enableFields(temp_fields)>
+                                        <script>bike_fields = add_bike_field(bike_fields, "sku");</script>
 
-                                        <div class="form-select mt-3" required>
-                                            <!--<label for="price">Price</label>-->
-                                            <input type="text" class="form-control" id="price" name="price" placeholder="Price">
-                                        </div>
+                                        <input type="text" class="form-select mt-3" id="part_num" name="part_num" placeholder="Part Number" oninput=enableFields(temp_fields)>
+                                        <script>bike_fields = add_bike_field(bike_fields, "part_num");</script>
 
-        
+                                        <input type="text" class="form-select mt-3" id="price" name="price" placeholder="Price">
+                                        <script>bike_fileds = add_bike_field(bike_fields, "price");
+                                                disableFields(bike_fields);
+                                                var temp_fields = bike_fields;
+                                        </script>
+                                       
 
-                                        
-
-
-
-            
                                     <div class="form-button mt-3">
                                         <button id="add_bike" type="submit" class="btn btn-primary" onclick="return foo();">Add Bike</button>
                                     </div>
@@ -204,7 +237,6 @@
                             </div>
                         </div>
                     </div>
-                <!--</div>-->
             </div>
         </form>
     </body>
